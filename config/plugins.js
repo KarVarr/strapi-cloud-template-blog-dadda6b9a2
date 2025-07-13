43f7d2718798699c07be68610425a0219d34b050
KarVarr <megatr9n@gmail.com>
2025-07-13 14:42:01 +0400

    added location 2

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = () => ({
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 1000000, // лимит в байтах (1MB), можешь увеличить при необходимости
+        },
+      },
+    },
+  });
+  
